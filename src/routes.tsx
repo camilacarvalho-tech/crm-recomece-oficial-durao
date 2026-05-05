26e9759c54250009494bfb5cca3f315b7e2ae1d7
Camila Carvalho <carvalhocarvalhoduraocamila@gmail.com>
2026-05-05 16:25:13 -0300

    routes fix final

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+function Clientes() {
+  return <h1>Clientes OK</h1>;
+}
+
+function Leads() {
+  return <h1>Leads OK</h1>;
+}
+
+function Sistema() {
+  return <h1>Sistema OK</h1>;
+}
+
+function Conversao() {
+  return <h1>Conversão OK</h1>;
+}
+
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Clientes OK</h1>} />
-        <Route path="/leads" element={<h1>Leads OK</h1>} />
-        <Route path="/sistema" element={<h1>Sistema OK</h1>} />
-        <Route path="/conversao" element={<h1>Conversão OK</h1>} />
+        <Route path="/" element={<Clientes />} />
+        <Route path="/leads" element={<Leads />} />
+        <Route path="/sistema" element={<Sistema />} />
+        <Route path="/conversao" element={<Conversao />} />
       </Routes>
     </BrowserRouter>
   );
